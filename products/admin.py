f5811759030a257792f337861733644c7b4fd19e
gomathishankar28 <gomathishankar28@gmail.com>
2021-09-25 12:07:12 +0000

    correcting runtime error

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,30 @@
-from django.contrib import admin
-from .models import Product, Category
+from django.db import models
 
-# Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
+class Category(models.Model):
 
-    ordering = ('sku',)
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
+    def __str__(self):
+        return self.name
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
